bd3e0d1d8362a3dc2ad7addf9a4057603dc8263b
Zefwork <38811556+Zefwork@users.noreply.github.com>
2023-04-28 19:57:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,3 @@ if streamlit.button('Get Fruit Load List'):
 
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-fruityvice_response = my_cur.fetchall()
